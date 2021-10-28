0459e321f6a73d33170b680fe2eac601fb1ce883
BastianVIA <70633697+BastianVIA@users.noreply.github.com>
2021-10-28 11:25:30 +0200

    Hej

---
+++ src/main/java/com/example/Tier3/Tier3Application.java
@@ -0,0 +1,14 @@
+package com.example.Tier3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tier3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tier3Application.class, args);
+		System.out.println("Hej");
+	}
+
+}
